38b9e5aacd3ed31a7834d2feadd204b449f6920e
molthorat <70627148+molthorat@users.noreply.github.com>
2021-01-19 14:01:19 +0530

    Add files via upload

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,7 +3,21 @@
 import java.util.*;
 public class TicTacToe{
 	public static void main(String ar[]){
+		char[][] gameBoard = {{' ','|',' ','|',' '},
+				{'-','+','-','+','-'},
+				{' ','|',' ','|',' '},
+				{'-','+','-','+','-'},
+				{' ','|',' ','|',' '}};
+		printGameBoard(gameBoard);
+	}
 
+	public static void printGameBoard(char[][] gameBoard){
+		for(char[] row : gameBoard){
+			for(char c : row){
+				System.out.print(c);
+			}
+			System.out.println();
+		}
 	}
 
 }
