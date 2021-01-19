f5261c53916eb4a5f58eac20c8999ee5cf72cc4d
molthorat <70627148+molthorat@users.noreply.github.com>
2021-01-19 14:25:12 +0530

    Add files via upload

--- TicTacToe.java
+++ TicTacToe.java
@@ -8,6 +8,20 @@ public class TicTacToe{
 				{' ','|',' ','|',' '},
 				{'-','+','-','+','-'},
 				{' ','|',' ','|',' '}};
+	
+		int playerPos = Integer.parseInt(inputPosition());			
+			
+		if(playerPos == 0){
+			System.out.println("Invalid input");
+		}
+		else if(playerPos == 11){
+			System.exit(0);
+		}else if(playerPos == 10){
+			//print scoreboard
+		}else{
+			System.out.println(playerPos);
+		}
+
 		printGameBoard(gameBoard);
 	}
 
@@ -18,6 +32,25 @@ public class TicTacToe{
 			}
 			System.out.println();
 		}
+	}
+
+	public static String inputPosition(){
+		Scanner sc = new Scanner(System.in);			
+		System.out.print("tictactoe>");
+		String input = sc.nextLine();				
+
+		if(input.equals("move 0,0")){return "1";}
+		else if(input.equals("move 0,2")){return "2";}
+		else if(input.equals("move 0,4")){return "3";}
+		else if(input.equals("move 2,0")){return "4";}
+		else if(input.equals("move 2,2")){return "5";}
+		else if(input.equals("move 2,4")){return "6";}
+		else if(input.equals("move 4,0")){return "7";}
+		else if(input.equals("move 4,2")){return "8";}
+		else if(input.equals("move 4,4")){return "9";}
+		else if(input.equals("scoreboard")){return "10";}
+		else if(input.equals("quite")){return "11";}
+		return "0";		
 	}
 
 }
